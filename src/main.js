2382b2e5da516b0728bfe60791f07a2d14624d8e
May Mossor <benjamin.mossor@student.swic.edu>
2025-10-29 15:01:37 -0500

    Add custom greeting support

--- src/main.js
+++ src/main.js
@@ -1,24 +1,47 @@
 // Variables - same as console JavaScript
 let clickCount = 0;
+const defaultMsg = "Hello, Console JavaScript!";
+const defaultUpdateMsg = "Hello, Browser JavaScript!";
 
 // Get references to HTML elements
 const greetingElement = document.getElementById("greeting");
 const counterButton = document.getElementById("change-greeting");
-const resetButton = document.getElementById("reset-count");
+const resetButton = document.getElementById("reset-form");
 const countElement = document.getElementById("click-count");
+const customGreetingMsg = document.getElementById("greeting-msg");
 
 // Function - same logic as console JavaScript
 function updateGreeting() {
   clickCount++;
-  greetingElement.textContent = "Hello, Browser JavaScript!";
+  greetingElement.textContent = defaultUpdateMsg;
   countElement.textContent = `Button clicks: ${clickCount}`;
+  if (clickCount === 5) {
+    greetingElement.style.color = "blue";
+  }
+  if (customGreetingMsg.value !== "") {
+    greetingElement.textContent = customGreetingMsg.value;
+  }
 }
 
-const resetCounter = () => {
+const resetForm = () => {
+  // Reset Click Counter
   clickCount = 0;
   countElement.textContent = `Button clicks: ${clickCount}`;
+  // Reset Greeting
+  greetingElement.style.color = "black";
+  greetingElement.textContent = defaultMsg;
+  // Reset Text Input
+  customGreetingMsg.value = "";
 };
+
+// const changeHeadingColor = () => {
+//   if (clickCount === 5) {
+//     greetingElement.color = "blue";
+//   }
+// if clickCount = 5
+// then changeHeadingColor to blue
+// };
 
 // Event listener - this is new! Responds to user interaction
 counterButton.addEventListener("click", updateGreeting);
-resetButton.addEventListener("click", resetCounter);
+resetButton.addEventListener("click", resetForm);
