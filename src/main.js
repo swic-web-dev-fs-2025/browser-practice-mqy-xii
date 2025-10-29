8b414b4630e95d35789ef917ec45716b34f9221d
May Mossor <benjamin.mossor@student.swic.edu>
2025-10-29 16:12:33 -0500

    Add decrease button

--- src/main.js
+++ src/main.js
@@ -4,15 +4,27 @@ let clickCount = 0;
 // Get HTML elements
 const counterElement = document.getElementById("counter");
 const increaseButton = document.getElementById("increase-btn");
+const decreaseButton = document.getElementById("decrease-btn");
+const resetButton = document.getElementById("reset-btn");
 
 // Functions
+// Display function
+const displayCounter = () => {
+  counterElement.textContent = `Counter: ${clickCount}`;
+};
+
 // Increase function
 const increaseCount = () => {
   clickCount++;
-  counterElement.textContent = `Counter: ${clickCount}`;
+  displayCounter();
 };
 // Decrease function
+const decreaseCount = () => {
+  clickCount--;
+  displayCounter();
+};
 // Reset function
 
 // Event listeners
 increaseButton.addEventListener("click", increaseCount);
+decreaseButton.addEventListener("click", decreaseCount);
