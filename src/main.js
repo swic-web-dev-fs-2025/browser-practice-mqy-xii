cdbf8bfdb7f812a37517beb4591b0fc8f03abfab
May Mossor <benjamin.mossor@student.swic.edu>
2025-10-29 16:28:52 -0500

    Add background color change when counter is 10 or greater

--- src/main.js
+++ src/main.js
@@ -13,20 +13,32 @@ const displayCounter = () => {
   counterElement.textContent = `Counter: ${clickCount}`;
 };
 
+// Change background color
+const changeBGcolor = () => {
+  if (clickCount >= 10) {
+    document.body.style.backgroundColor = "red";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+};
+
 // Increase function
 const increaseCount = () => {
   clickCount++;
   displayCounter();
+  changeBGcolor();
 };
 // Decrease function
 const decreaseCount = () => {
   clickCount--;
   displayCounter();
+  changeBGcolor();
 };
 // Reset function
 const resetCount = () => {
   clickCount = 0;
   displayCounter();
+  changeBGcolor();
 };
 
 // Event listeners
